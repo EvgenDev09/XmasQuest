79bdc23f3bee35a64cff1e20dd3557ea7867256d
Evgen09 <eugene.lippa@gmail.com>
2023-12-29 21:44:25 +0300

    Added More Words and Color

--- Happy.py
+++ Happy.py
@@ -1,38 +1,73 @@
+import os
+import time
+
 def print_letter(letter):
     if letter == 'H':
-        return ['*   *', '*   *', '*****', '*   *', '*   *']
+        return ['█   █', '█   █', '█████', '█   █', '█   █']
     elif letter == 'A':
-        return [' *** ', '*   *', '*****', '*   *', '*   *']
+        return [' ███ ', '█   █', '█████', '█   █', '█   █']
     elif letter == 'P':
-        return ['**** ', '*   *', '**** ', '*    ', '*    ']
+        return ['████ ', '█   █', '████ ', '█    ', '█    ']
     elif letter == 'Y':
-        return ['*   *', ' * * ', '  *  ', '  *  ', '  *  ']
+        return ['█   █', ' █ █ ', '  █  ', '  █  ', '  █  ']
     elif letter == 'N':
-        return ['*   *', '**  *', '* * *', '*  **', '*   *']
+        return ['█   █', '██  █', '█ █ █', '█  ██', '█   █']
     elif letter == 'E':
-        return ['*****', '*    ', '*****', '*    ', '*****']
+        return ['█████', '█    ', '█████', '█    ', '█████']
     elif letter == 'W':
-        return ['*   *', '*   *', '* * *', '** **', '*   *']
+        return ['█   █', '█   █', '█ █ █', '██ ██', '█   █']
     elif letter == 'R':
-        return ['**** ', '*   *', '**** ', '* *  ', '*   *']
+        return ['████ ', '█   █', '████ ', '█ █  ', '█   █']
+    elif letter == '0':
+        return [' ███ ', '█  ██', '█ █ █', '██  █', ' ███ ']
+    elif letter == '2':
+        return [' ███ ', '█   █', '  ██ ', ' █   ', '█████']
+    elif letter == '3':
+        return ['████ ', '    █', '  ██ ', '    █', '████ ']
+    elif letter == '4':
+        return ['█  █ ', '█  █ ', '█████', '   █ ', '   █ ']
     else:
         return ['     ', '     ', '     ', '     ', '     ']
 
-def print_word(word):
+def print_word(word, color):
+    print(f'\x1b[1;{color};40m', end='')
     for i in range(5):
         for letter in word:
             print(print_letter(letter)[i], end='  ')
         print()
 
-print_word('HAPPY')
+print_word('HAPPY', 31)
 print()
-print_word('NEW')
+time.sleep(1)
+print_word('NEW', 32)
 print()
-print_word('YEAR')
-
-
-print("   *   ")
-print("  ***  ")
-print(" ***** ")
-print("*******")
-print("  ***  ")
+time.sleep(1)
+print_word('YEAR', 33)
+print()
+time.sleep(1)
+for i in range(5):
+    print(f'\x1b[1;36;40m', end='')
+    for letter in "202":
+        print(print_letter(letter)[i], end='  ')
+    print(f'\x1b[1;31;40m', end='')
+    print(print_letter("3")[i])
+time.sleep(1)
+for j in range(5):
+    os.system('cls')
+    print_word('HAPPY', 31)
+    print()
+    print_word('NEW', 32)
+    print()
+    print_word('YEAR', 33)
+    print()
+    for i in range(5):
+        print(f'\x1b[1;36;40m', end='')
+        for letter in "202":
+            print(print_letter(letter)[i], end='  ')
+        if i <= j:
+            print(print_letter("4")[4 - j + i])
+        else:
+            print(f'\x1b[1;31;40m', end='')
+            print(print_letter("3")[i - j])
+    time.sleep(0.2)
+time.sleep(2)
