b5137c23e1cfd5dd2a565fa39539c0ee9f31da4a
COOL BOY <154074810+blessedBLE@users.noreply.github.com>
2023-12-29 21:05:10 +0300

    Update Happy.py

--- Happy.py
+++ Happy.py
@@ -1,8 +1,34 @@
-print("  *  *  *****  *****  *    *     **    ****  *    *  *****  *    *")
-print("  *  *  *      *      *    *    *  *   *   *  **   *  *      **   *")
-print("  ****  ****   ****   *    *   ******  ****   * *  *  ****   * *  *")
-print("  *  *  *      *      *    *  *      * * *    *  * *  *      *  * *")
-print("  *  *  *****  *****  *****  *      * *  **  *   **  *****  *   **")
+def print_letter(letter):
+    if letter == 'H':
+        return ['*   *', '*   *', '*****', '*   *', '*   *']
+    elif letter == 'A':
+        return [' *** ', '*   *', '*****', '*   *', '*   *']
+    elif letter == 'P':
+        return ['**** ', '*   *', '**** ', '*    ', '*    ']
+    elif letter == 'Y':
+        return ['*   *', ' * * ', '  *  ', '  *  ', '  *  ']
+    elif letter == 'N':
+        return ['*   *', '**  *', '* * *', '*  **', '*   *']
+    elif letter == 'E':
+        return ['*****', '*    ', '*****', '*    ', '*****']
+    elif letter == 'W':
+        return ['*   *', '*   *', '* * *', '** **', '*   *']
+    elif letter == 'R':
+        return ['**** ', '*   *', '**** ', '* *  ', '*   *']
+    else:
+        return ['     ', '     ', '     ', '     ', '     ']
+
+def print_word(word):
+    for i in range(5):
+        for letter in word:
+            print(print_letter(letter)[i], end='  ')
+        print()
+
+print_word('HAPPY')
+print()
+print_word('NEW')
+print()
+print_word('YEAR')
 
 
 print("   *   ")
